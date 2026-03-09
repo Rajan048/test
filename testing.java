5c086e2080f7b17f862cb2a9ca871dd93c92c94d
Rajan048 <rajansaini2.0.0.6@gmail.com>
2026-03-09 23:32:23 +0530

    [AI-AGENT] Fix logic issues and syntax errors

--- testing.java
+++ testing.java
@@ -1,8 +1,8 @@
-public class Testing {
- public static void main(String[] args) {
-  int a = 10;
-  int b = 20;
-  int sum = a + b;
-  System.out.println(sum);
- }
+class Testing { 
+    public static void main(String[] args) { 
+        int a = 10; 
+        int b = 20; 
+        int sum = a + b; 
+        System.out.println(sum); 
+    } 
 }
