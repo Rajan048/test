6463186c19c55489d5b2a4a378d83216ca507fac
Rajan Saini <rajansaini2.0.0.6@gmail.com>
2026-03-09 23:48:05 +0530

    Rename variable 'sum' to 'sm' in Testing class

--- testing.java
+++ testing.java
@@ -2,7 +2,7 @@ class Testing {
     public static void main(String[] args) { 
         int a = 10; 
         int b = 20; 
-        int sum = a + b; 
+        int sm = a + b; 
         System.out.println(sum); 
     } 
 }
